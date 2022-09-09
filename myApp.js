f4e0951b7cbc483089f9a1e0b5e97826c4f6d87d
Julio Morales <47278600+jamorales11@users.noreply.github.com>
2022-09-09 02:31:21 +0000

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -3,14 +3,27 @@ let app = express();
 
 console.log("Hello World")
 
+/* 
+In the first two lines of the file myApp.js, you can see how easy it is to create an Express app object. This object has several methods, and you will learn many of them in these challenges. One fundamental method is app.listen(port). It tells your server to listen on a given port, putting it in running state. For testing reasons, we need the app to be running in the background so we added this method in the server.js file for you.
 
-app.get("/", (req,res) =>{
-  res.send("Hello Express")
+Let’s serve our first string! In Express, routes takes the following structure: app.METHOD(PATH, HANDLER). METHOD is an http method in lowercase. PATH is a relative path on the server (it can be a string, or even a regular expression). HANDLER is a function that Express calls when the route is matched. Handlers take the form function(req, res) {...}, where req is the request object, and res is the response object. For example, the handler will serve the string 'Hello Express' as response string. 
+*/
+
+//app.get("/", (req,res) =>{
+//  res.send("Hello Express")
+//})
+
+
+
+/*
+You can respond to requests with a file using the res.sendFile(path) method. You can put it inside the app.get('/', ...) route handler. Behind the scenes, this method will set the appropriate headers to instruct your browser on how to handle the file you want to send, according to its type. Then it will read and send the file. This method needs an absolute file path. We recommend you to use the Node global variable __dirname to calculate the path like this:
+absolutePath = __dirname + relativePath/file.ext
+*/
+
+
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/views/index.html")
 })
-
-
-
-
 
 
 
