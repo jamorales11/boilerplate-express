3caefe5e7c473e6302dab699acbd5609bdde06a9
Julio Morales <47278600+jamorales11@users.noreply.github.com>
2022-09-09 03:29:45 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -27,9 +27,16 @@ app.get("/", (req,res) => {
 
 
 
+/*
+An HTML server usually has one or more directories that are accessible by the user. You can place there the static assets needed by your application (stylesheets, scripts, images).
+
+In Express, you can put in place this functionality using the middleware express.static(path), where the path parameter is the absolute path of the folder containing the assets.
+
+If you don’t know what middleware is... don’t worry, we will discuss in detail later. Basically, middleware are functions that intercept route handlers, adding some kind of information. A middleware needs to be mounted using the method app.use(path, middlewareFunction). The first path argument is optional. If you don’t pass it, the middleware will be executed for all requests.
+*/
 
 
-
+app.use("/public", express.static(__dirname + "/public"))
 
 
 
