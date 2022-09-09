0858b3c290bc92302d61023b6b7b7dbedd422575
Julio Morales <47278600+jamorales11@users.noreply.github.com>
2022-09-09 03:40:04 +0000

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ let app = express();
 
 console.log("Hello World")
 
-/* 
+/* Start a working Express Server
+
 In the first two lines of the file myApp.js, you can see how easy it is to create an Express app object. This object has several methods, and you will learn many of them in these challenges. One fundamental method is app.listen(port). It tells your server to listen on a given port, putting it in running state. For testing reasons, we need the app to be running in the background so we added this method in the server.js file for you.
 
 Let’s serve our first string! In Express, routes takes the following structure: app.METHOD(PATH, HANDLER). METHOD is an http method in lowercase. PATH is a relative path on the server (it can be a string, or even a regular expression). HANDLER is a function that Express calls when the route is matched. Handlers take the form function(req, res) {...}, where req is the request object, and res is the response object. For example, the handler will serve the string 'Hello Express' as response string. 
@@ -15,7 +16,8 @@ Let’s serve our first string! In Express, routes takes the following structure
 
 
 
-/*
+/*Serve an HTML File
+
 You can respond to requests with a file using the res.sendFile(path) method. You can put it inside the app.get('/', ...) route handler. Behind the scenes, this method will set the appropriate headers to instruct your browser on how to handle the file you want to send, according to its type. Then it will read and send the file. This method needs an absolute file path. We recommend you to use the Node global variable __dirname to calculate the path like this:
 absolutePath = __dirname + relativePath/file.ext
 */
@@ -27,7 +29,8 @@ app.get("/", (req,res) => {
 
 
 
-/*
+/*Serve Static Assets
+
 An HTML server usually has one or more directories that are accessible by the user. You can place there the static assets needed by your application (stylesheets, scripts, images).
 
 In Express, you can put in place this functionality using the middleware express.static(path), where the path parameter is the absolute path of the folder containing the assets.
@@ -39,9 +42,17 @@ If you don’t know what middleware is... don’t worry, we will discuss in deta
 app.use("/public", express.static(__dirname + "/public"))
 
 
+/*
+Serve JSON on a Specific Route
 
+While an HTML server serves HTML, an API serves data. A REST (REpresentational State Transfer) API allows data exchange in a simple way, without the need for clients to know any detail about the server. The client only needs to know where the resource is (the URL), and the action it wants to perform on it (the verb). The GET verb is used when you are fetching some information, without modifying anything. These days, the preferred data format for moving information around the web is JSON. Simply put, JSON is a convenient way to represent a JavaScript object as a string, so it can be easily transmitted.
 
+Let's create a simple API by creating a route that responds with JSON at the path /json. You can do it as usual, with the app.get() method. Inside the route handler, use the method res.json(), passing in an object as an argument. This method closes the request-response loop, returning the data. Behind the scenes, it converts a valid JavaScript object into a string, then sets the appropriate headers to tell your browser that you are serving JSON, and sends the data back. A valid object has the usual structure {key: data}. data can be a number, a string, a nested object or an array. data can also be a variable or the result of a function call, in which case it will be evaluated before being converted into a string.
+*/
 
+app.get("/json", (req,res) => {
+  res.json({"message":"Hello json"})
+})
 
 
 
